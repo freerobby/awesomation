cafa69c812e94f1cea1bc0ec59ae080d53c22090
Robby Grossman <robby@freerobby.com>
2010-11-23 15:09:08 -0500

    Add support for HTTP basic authentication.

--- app.rb
+++ app.rb
@@ -2,17 +2,34 @@ require 'sinatra'
 
 require 'hrgz1'
 
+helpers do
+  def protected!
+    unless ENV['BASICAUTH_USERNAME'].blank? || ENV['BASICAUTH_PASSWORD'].blank? # Disregard protection if no user/pass specified
+      if !authorized?
+        response['WWW-Authenticate'] = %(Basic realm="Testing HTTP Auth")
+        throw(:halt, [401, "Not authorized\n"])
+      end
+    end
+  end
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == [ENV['BASICAUTH_USERNAME'], ENV['BASICAUTH_PASSWORD']]
+  end
+end
+
 get '/' do
   "Awesomation is running!"
 end
 
 # Turn a node on
 get '/appliances/:node_id/on' do
+  protected!
   new_gateway.turn_appliance_on(params[:node_id])
 end
 
 # Turn a node off
 get '/appliances/:node_id/off' do
+  protected!
   new_gateway.turn_appliance_off(params[:node_id])
 end
 
